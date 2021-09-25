5e95a349f4602c554896b3b61cf63de5d7a76b88
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-25 20:28:26 +1200

    added header text and p text to main Preveiw

--- client/components/MainPreview.jsx
+++ client/components/MainPreview.jsx
@@ -3,8 +3,22 @@ import React from 'react'
 
 const MainPreview = () => {
 
+    const text = 'When a young boy vanishes, a small town uncovers a mystery involving sercret experiments , terrifying supernatural forces & a strange little girl.'
+
+
+
     return (
         <div className="main-preview-container">
+            
+            <div className="main-preview-header-container">
+                <div className="main-preview-header">
+                    <h1>Stranger Things</h1>
+                </div>
+                <div className="main-preview-text"> 
+                    <p>{text}</p>
+                </div>
+            </div>
+
             <img src='/bg.jpeg' alt='Stranger Things image' className='main-preview-img' />
         </div>
     )

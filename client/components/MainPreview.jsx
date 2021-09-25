ecfd0e394960f6b3501a0df234a5ecdc4f3d8717
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-25 20:04:32 +1200

    created components for build

---
+++ client/components/MainPreview.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const MainPreview = () => {
+
+    return (
+        <div>
+            <h1>Main Preview</h1>
+        </div>
+    )
+}
+
+
+export default MainPreview

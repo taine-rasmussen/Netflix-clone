986d1b743718cd51901fc84d83548f7da9c62aad
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-27 19:04:50 +1300

    changed preview class names

--- client/components/Preview.jsx
+++ client/components/Preview.jsx
@@ -5,29 +5,29 @@ const Preview = () => {
 
     return (
         <div className="preview-container">
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
-            <div className='preview-img'>
-                <img src="preview1.jpeg" alt="tv show poster"/>
+            <div className='preview-img-container'>
+                <img src="preview1.jpeg" className="preview-img" alt="tv show poster"/>
             </div>
         </div>
     )

ab3e322484a366659d56f12e34ab57b2844d7b5f
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-02 14:16:08 +1300

    added search icon

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,16 +3,10 @@ import React from 'react'
 
 const Nav = () => {
 
-
-            // spend some time researching css properties that handle responsiveness to clean up what we are currently doing.
-
     return (
         <div className="nav-container">
-            <ul>Search</ul>
-            <ul>Home</ul>
-            <ul>Browse</ul>
-            <ul>TV shows</ul>
-            <ul>Movies</ul>
+            <ul className="fa fa-search  search-icon"></ul>
+        
         </div>
     )
 }
